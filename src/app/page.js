2eca03649aec7ce2264cb5208a05aae732ae0419
MisterCxmpy <cosgunhussein@gmail.com>
2023-12-29 11:34:51 +0000

    styling and layout changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,24 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import prestineImage from '../../public/static/images/Prestine POS Application.png';
 
 export default function Home() {
   return (
     <div className={styles['main']}>
-      <div className={`${styles['hero']} fx-center fx-column`}>
+      <section className={`${styles['hero']} fx-center fx-column`}>
         <h1 className={styles['title']}>Prestine: Elevate Efficiency</h1>
         <h2 className={styles['sub-heading']}>Simplify, Boost Efficiency, Delight Customers with Prestine.</h2>
         <div className={styles['call-to-action']}>
           <button className={styles['action-1']}>Learn more</button>
           <button className={styles['action-2']}>Read Documentation</button>
         </div>
-      </div>
+        
+      </section>
+      <Image
+        src={prestineImage}
+        alt="Prestine POS Application"
+        className={styles['image']}
+      />
     </div>
   )
 }
--- src/layouts/Navbar/index.js
+++ src/layouts/Navbar/index.js
@@ -1,26 +1,49 @@
+"use client"
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from './index.module.css';
 
 export default function Navbar() {
+  const [isNavbarFixed, setIsNavbarFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const threshold = 20;
+
+      setIsNavbarFixed(scrollPosition > threshold);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className={`${styles['navbar']} fx-center`}>
-      <div className={styles['title']}>
-        <Link href="/">
-          <span className={`${styles['nav-link']} ${styles["title-text"]}`}>Prestine</span>
-        </Link>
-      </div>
-      <Link href="/pricing">
-        <span className={styles['nav-link']}>Pricing</span>
-      </Link>
-      <Link href="/features">
-        <span className={styles['nav-link']}>Features</span>
-      </Link>
-      <Link href="/documentation">
-        <span className={styles['nav-link']}>Documentation</span>
-      </Link>
-      <Link href="/changelog">
-        <span className={styles['nav-link']}>Change Log</span>
-      </Link>
-    </nav>
+    <header className={`${styles['header']} ${isNavbarFixed ? styles['scrolled'] : ''} fx-center`}>
+      <nav className={`${styles['container']}`}>
+        <div className={`${styles['items']} fx-center`}>
+          <div className={styles['title']}>
+            <Link href="/">
+              <span className={`${styles['nav-link']} ${styles["title-text"]}`}>Prestine</span>
+            </Link>
+          </div>
+          <Link href="/pricing">
+            <span className={styles['nav-link']}>Pricing</span>
+          </Link>
+          <Link href="/features">
+            <span className={styles['nav-link']}>Features</span>
+          </Link>
+          <Link href="/documentation">
+            <span className={styles['nav-link']}>Documentation</span>
+          </Link>
+          <Link href="/changelog">
+            <span className={styles['nav-link']}>Change Log</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 }
